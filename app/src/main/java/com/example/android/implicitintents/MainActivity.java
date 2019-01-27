d4cfa5312aeda5234f5e27e2b6d2a39b2dc2bb6d
kevinandrianliu <39858143+kevinandrianliu@users.noreply.github.com>
2019-01-27 19:04:45 +0700

    Implement share this text

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.implicitintents;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +12,17 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebsiteEditText = (EditText) findViewById(R.id.website_edittext);
         mLocationEditText = (EditText) findViewById(R.id.location_edittext);
+        mShareTextEditText = (EditText) findViewById(R.id.share_edittext);
     }
+
     public void openWebsite(View view) {
         String url = mWebsiteEditText.getText().toString();
         Uri webpage = Uri.parse(url);
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("ImplicitIntents", "Can't handle this!");
         }
     }
+
     public void openLocation(View view) {
         String loc = mLocationEditText.getText().toString();
         Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
@@ -37,5 +43,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.d("ImplicitIntents", "Can't handle this intent!");
         }
+    }
+
+    public void shareThis(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
     }
 }
